36d5a0c5bc94236f85b933c6e00ac935cd6b9173
or4acle <sixxematty1124@gmail.com>
2026-06-25 15:40:40 -0300

    variaveis ebaa
    variaveis (based kotlin edition)

---
+++ Variaveis.java
@@ -0,0 +1,28 @@
+// ebaaa variaveis q legau manerãoooooo
+// 25/06/2026
+
+fun main(){
+    val isSigma = true; // vai aparecer q é sigma
+    val isGay = false;
+    val idade: Int = 13;
+    val nome: String = "or4acle";
+    val personagens: String = "futaba e zero two";
+    val comida: String = "parmegiana de frango";
+    val jogo: String = "persona 5 royal";
+    val gayWtf: String = "é gay!!";
+
+    println("Ah, olá! Você deve ser o " + nome + "... né?");
+    println("Eu vou fazer algumas adivinhações...");
+    println("Você tem " + idade + ";");
+    println("Suas personagens favoritas são a " + personagens + ";");
+    println("Sua comida favorita é " + comida + ";");
+    println("Seu jogo favorito é " + jogo + ";");
+    if (isGay){
+        println("E você é gay!!!!!!!");
+    }
+    else{
+        println("E você NÃO é gay!!!!!!!!");
+    }
+
+
+}
